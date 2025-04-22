54bb4849fa2541fa5fc2f01a2c6afb8e53195448
Oskar Jabłko <oskar.jablko@hoyergroup.onmicrosoft.com>
2025-04-22 14:14:35 +0200

    refactor: implement async handling for getAllTours and getTour methods in tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,23 +1,41 @@
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    requested_at: req.requestTime,
-    status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      requested_at: req.requestTime,
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'failed bruh',
+    });
+  }
 };
 
-exports.getTour = (req, res) => {
-  res.status(200).json({
-    // status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'failed bruh',
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -33,7 +51,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'invalid data sent!'
+      message: 'invalid data sent!',
     });
   }
 };
