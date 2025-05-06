ba8beaa1a1067bd250e3889e24f600a9ee67e50a
jablkoskar <oskar.jablko84@gmail.com>
2025-05-06 18:29:59 +0200

    implement sorting functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,9 +10,17 @@ exports.getAllTours = async (req, res) => {
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     console.log(JSON.parse(queryStr));
-    
-    const query = await Tour.find(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
     //chaining filtering methods to be done here
+
+    //SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     //EXECUTE QUERY
     const tours = await query;
